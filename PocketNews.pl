b9ae7cd41433b0381065710dfe20194bc88dbdc5
Nedyalko Dyakov <n3mo.wolf@gmail.com>
2013-01-31 01:26:28 +0200

    Expecting configuration file from cli

--- PocketNews.pl
+++ PocketNews.pl
@@ -7,10 +7,12 @@ use Data::Dumper;
 $\ = "\n";
 
 sub ReadConfig{
-   my $xml = new XML::Simple;
-   my $cfg = $xml->XMLin("default.conf", KeyAttr => { block => 'type', item => 'name' }, ForceArray => [ 'block', 'item' ]);
-   return $cfg if CheckConfig($cfg) or die("ERROR IN CONF FILE");
+    my $cfgfile = shift;
+    my $xml = new XML::Simple;
+    my $cfg = $xml->XMLin($cfgfile, KeyAttr => { block => 'type', item => 'name' }, ForceArray => [ 'block', 'item' ]);
+    return $cfg if CheckConfig($cfg) or die("ERROR IN CONF FILE : $cfgfile");
 }
+
 sub CheckConfig{
     my $cfg = shift;
     my $rss = $cfg->{block}->{rss}->{item} if defined $cfg->{block}->{rss}->{item};
@@ -18,7 +20,15 @@ sub CheckConfig{
     return 1 if (ref($cfg) eq 'HASH' && exists $cfg->{block}->{system} && exists $cfg->{block}->{system}->{item}->{DBFILE} 
     && ( $cfg->{block}->{system}->{item}->{WEATHER}->{content} eq 0 || ( $cfg->{block}->{system}->{item}->{WEATHER}->{content} eq 1 && $cfg->{block}->{system}->{item}->{LOCATION} ) ) && $#$tags >= 0 && $#$rss >= 0);
 }
-my $cfg = ReadConfig;
-my $db = PocketNews::DB->new( _filename => $cfg->{block}->{system}->{item}->{DBFILE}->{content});
-
+MAIN: 
+{
+    my $cfgfile;
+    if($#ARGV >= 0){ 
+        $cfgfile = $ARGV[0];
+    }else{ 
+        $cfgfile = "default.conf";
+    }
+    my $cfg = ReadConfig($cfgfile);
+    my $db = PocketNews::DB->new( _filename => $cfg->{block}->{system}->{item}->{DBFILE}->{content});
+}
 
