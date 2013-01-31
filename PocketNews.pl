99b742393e712fc3a85491027785d975ab664232
Nedyalko Dyakov <n3mo.wolf@gmail.com>
2013-01-31 02:26:01 +0200

    Changing configuration parsing a little bit...

--- PocketNews.pl
+++ PocketNews.pl
@@ -9,16 +9,16 @@ $\ = "\n";
 sub ReadConfig{
     my $cfgfile = shift;
     my $xml = new XML::Simple;
-    my $cfg = $xml->XMLin($cfgfile, KeyAttr => { block => 'type', item => 'name' }, ForceArray => [ 'block', 'item' ]);
+    my $cfg = $xml->XMLin($cfgfile, KeyAttr => { block => 'type' }, ValueAttr => ['value'], ForceArray => [ 'block', 'item', 'link']);
     return $cfg if CheckConfig($cfg) or die("ERROR IN CONF FILE : $cfgfile");
 }
 
 sub CheckConfig{
     my $cfg = shift;
-    my $rss = $cfg->{block}->{rss}->{item} if defined $cfg->{block}->{rss}->{item};
-    my $tags = $cfg->{block}->{tag}->{item} if defined $cfg->{block}->{tag}->{item};
-    return 1 if (ref($cfg) eq 'HASH' && exists $cfg->{block}->{system} && exists $cfg->{block}->{system}->{item}->{DBFILE} 
-    && ( $cfg->{block}->{system}->{item}->{WEATHER}->{content} eq 0 || ( $cfg->{block}->{system}->{item}->{WEATHER}->{content} eq 1 && $cfg->{block}->{system}->{item}->{LOCATION} ) ) && $#$tags >= 0 && $#$rss >= 0);
+    my $rss = $cfg->{block}->{rss}->{link} if defined $cfg->{block}->{rss}->{link};
+    my $tags = $cfg->{block}->{tags}->{tag} if defined $cfg->{block}->{tags}->{tag};
+    return 1 if (ref($cfg) eq 'HASH' && exists $cfg->{block}->{system} && exists $cfg->{block}->{system}->{DBFILE} 
+    && ( $cfg->{block}->{system}->{WEATHER} eq 0 || ( $cfg->{block}->{system}->{WEATHER} eq 1 && $cfg->{block}->{system}->{LOCATION} ) ) && $#$tags >= 0 && $#$rss >= 0);
 }
 MAIN: 
 {
@@ -29,6 +29,7 @@ MAIN:
         $cfgfile = "default.conf";
     }
     my $cfg = ReadConfig($cfgfile);
-    my $db = PocketNews::DB->new( _filename => $cfg->{block}->{system}->{item}->{DBFILE}->{content});
+    print Dumper($cfg);
+    my $db = PocketNews::DB->new( _filename => $cfg->{block}->{system}->{DBFILE}->{content});
 }
 
